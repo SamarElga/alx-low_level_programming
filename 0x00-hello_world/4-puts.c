10d946f9a78f9e3aedf603b828ee7483cbf783cf
Samar <samarelga@gmail.com>
2023-05-01 04:56:56 -0700

    creating a c file that prints a text into an executable a.out file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - main entry point
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+/**
+* Return : 0 always (success)
+*/
+	return(0);
+}
