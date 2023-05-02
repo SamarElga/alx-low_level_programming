f72d17372fc6fc3001622921748b7ba586042fd7
Samar <samarelga@gmail.com>
2023-05-02 13:43:25 -0700

    writing a function that returns the length of a string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * swap_int - Swaps the values of two integers.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swapped.
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
 void swap_int(int *a, int *b)
 {
-	int tmp = *a;
+	int m;
+
+	m = *a;
 	*a = *b;
-	*b = tmp;
+	*b = m;
 }
